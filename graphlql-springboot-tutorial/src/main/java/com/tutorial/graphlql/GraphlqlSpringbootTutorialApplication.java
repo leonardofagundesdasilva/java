9be80ada122282ddcfa17bd2fc61638bbb20c757
Leo Fagundes <leonardo.fagundes@yahoo.com.br>
2025-06-16 14:33:52 -0700

    Adding the graphql-springboot-tutorial folder

---
+++ graphlql-springboot-tutorial/src/main/java/com/tutorial/graphlql/GraphlqlSpringbootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.graphlql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphlqlSpringbootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphlqlSpringbootTutorialApplication.class, args);
+	}
+
+}
---
+++ graphlql-springboot-tutorial/src/test/java/com/tutorial/graphlql/GraphlqlSpringbootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.graphlql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphlqlSpringbootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
