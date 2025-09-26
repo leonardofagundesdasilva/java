990ac33fc2acc7765324ba548e0dd2044d08c307
Leo Fagundes <leonardo.fagundes@yahoo.com.br>
2025-09-26 11:36:15 -0700

    Java 17 tests

--- java-tests/src/Main.java
+++ java-tests/src/Main.java
@@ -6,4 +6,23 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
     }
+
+    public String longestCommonPrefix(String[] strs) {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        outer: for(int i=0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            for(int j=1; j < strs.length; j++) {
+                if(i >= strs[j].length()
+                        || c != strs[j].charAt(i)) {
+                    break outer;
+                }
+            }
+
+            stringBuffer.append(c);
+        }
+
+        return stringBuffer.toString();
+    }
 }
